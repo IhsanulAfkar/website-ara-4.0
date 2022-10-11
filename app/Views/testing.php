225f00e740fe33d341e8529830425601d12f04eb
M Ihsanul Afkar <ihsanul2001@gmail.com>
2022-10-11 23:12:45 +0700

    testing

---
+++ app/Views/testing.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url(); ?>/css/template.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 class="text-3xl">ini testing</h1>
+    <div class="m-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ratione repellat blanditiis architecto iste iure quaerat at dolore voluptatum eaque.</div>
+</body>
+
+</html>

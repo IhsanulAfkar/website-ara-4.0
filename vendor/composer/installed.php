ea36177c85bf113bd627bca60a13abd94d0ebeb3
M Ihsanul Afkar <ihsanul2001@gmail.com>
2022-10-12 17:45:58 +0700

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v4.2.6',
+    'version' => '4.2.6.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'codeigniter4/appstarter',
+  ),
+  'versions' => 
+  array (
+    'codeigniter4/appstarter' => 
+    array (
+      'pretty_version' => 'v4.2.6',
+      'version' => '4.2.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'codeigniter4/framework' => 
+    array (
+      'pretty_version' => 'v4.2.6',
+      'version' => '4.2.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '872c8fb9d70dc1dbd8e4a85a5cf49d4fe2735525',
+    ),
+    'kint-php/kint' => 
+    array (
+      'pretty_version' => '4.2.3',
+      'version' => '4.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7601bfd95ccc50a1b903c2764b31d00919e8edd9',
+    ),
+    'laminas/laminas-escaper' => 
+    array (
+      'pretty_version' => '2.10.0',
+      'version' => '2.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '58af67282db37d24e584a837a94ee55b9c7552be',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+  ),
+);

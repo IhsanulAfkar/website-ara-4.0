89f6bff8c1ebf8e5d987d33965d11258156531d1
M Ihsanul Afkar <ihsanul2001@gmail.com>
2022-10-26 20:36:55 +0700

    coming soon

---
+++ app/Views/coming_soon.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url(); ?>/src/css/coming_soon/coming_soon.css.min">
+    <title>Coming Soon</title>
+</head>
+
+<body>
+    <div class="top_star">
+        <img src="<?= base_url(); ?>/coming_soon/yellow_star.png" class="yellow_star1">
+        <img src="<?= base_url(); ?>/coming_soon/blue star.png" class="blue_star1">
+    </div>
+    <div class="container_body font-Poppins">
+        <div class="container_div font-poppins">
+            <div class="coming_div">
+                <span class="coming_text">coming</span>
+                <div class="play_circle">
+                    <img src="<?= base_url(); ?>/coming_soon/play circle.png">
+                </div>
+                <div class="red_star1">
+                    <img src="<?= base_url(); ?>/coming_soon/red star.png">
+                </div>
+                <div class="arrow">
+                    <img src="<?= base_url(); ?>/coming_soon/arrow.png">
+                </div>
+            </div>
+            <div class="soon_div">
+                <img src="<?= base_url(); ?>/coming_soon/eclipse slider.png" class="slider">
+                <span class="soon">soon</span>
+                <img src="<?= base_url(); ?>/coming_soon/green eclipse.png" class="eclipse1">
+                <img src="<?= base_url(); ?>/coming_soon/yellow circle.png" class="yellow_circle1">
+            </div>
+            <div class="ara_div">
+                <img src="<?= base_url(); ?>/coming_soon/smile.png" class="smile1">
+                <span class="ara">ARA 4.0</span>
+                <img src="<?= base_url(); ?>/coming_soon/blue circle.png" class="blue_circle1">
+            </div>
+            <div class="icons_div">
+                <img src="<?= base_url(); ?>/coming_soon/blue star.png" alt="" class="blue_star2">
+                <div class="sosmed">
+                    <a href="https://www.instagram.com/ara_its/">
+                        <img src="<?= base_url(); ?>/coming_soon/sosmed/ig.png" alt="instagram">
+                    </a>
+                </div>
+                <div class="sosmed">
+                    <a href="https://www.youtube.com/channel/UC0jutwJdiQ_MRNSJG0CRYGA">
+                        <img src="<?= base_url(); ?>/coming_soon/sosmed/yt.png" alt="youtube">
+                    </a>
+                </div>
+                <div class="sosmed">
+                    <a href="https://twitter.com/ara__its">
+                        <img src="<?= base_url(); ?>/coming_soon/sosmed/twitter.png" alt="twitter">
+                    </a>
+                </div>
+                <div class="sosmed">
+                    <a href="https://web.facebook.com/profile.php?id=100086878021327&is_tour_completed=true">
+                        <img src="<?= base_url(); ?>/coming_soon/sosmed/fb.png" alt="facebook">
+                    </a>
+                </div>
+                <div class="sosmed">
+                    <a href="https://www.tiktok.com/@ara_its">
+                        <img src="<?= base_url(); ?>/coming_soon/sosmed/tiktok.png" alt="tiktok">
+                    </a>
+                </div>
+                <img src="<?= base_url(); ?>/coming_soon/blue star.png" alt="" class="blue_star3">
+            </div>
+            <div class="xl:hidden">
+                <img src="<?= base_url(); ?>/coming_soon/smile.png" class="mx-auto" width="100px">
+            </div>
+        </div>
+
+        <div class="xl:hidden">
+            <img src="<?= base_url(); ?>/coming_soon/half_star.png" class="half_star1">
+            <img src="<?= base_url(); ?>/coming_soon/quarter_circle.png" class="quarter_circle">
+        </div>
+        <div class="hidden xl:block">
+            <img src="<?= base_url(); ?>/coming_soon/half circle1.png" class="half_circle1">
+            <img src="<?= base_url(); ?>/coming_soon/half circle2.png" class="half_circle2">
+        </div>
+    </div>
+</body>
+
+</html>

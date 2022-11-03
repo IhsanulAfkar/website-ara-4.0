d10c9365f72d8b2973675531979066ebe7feaae8
ranggaaldosas <ranggaaldosas@gmail.com>
2022-11-03 14:32:29 +0700

    add abouthmit

---
+++ app/Views/landing/pages/abouthmit.php
@@ -0,0 +1,139 @@
+<!-- Section 1 -->
+<div>
+    <div class="flex items-center justify-center xl:flex-col xl:relative">
+      <img src="/abouthmit/halfcircle.png" alt="">
+      <img class="absolute right-0 top-0 invisible sm:visible" src="/abouthmit/rightline.png" alt="">
+    </div>
+
+    <div class="flex xl:flex-row flex-col-reverse mx-auto align-center bg-cover bg-no-repeat mb-16">
+
+      <!-- Left Side -->
+      <div class="flex flex-col w-1/2 mx-auto xl:p-12 xl:ml-12 items-center xl:items-start">
+        <img class="invisible xl:visible xl:relative xl:left-28" src="/abouthmit/chatyellow.png" alt="">
+        <div>
+          <h1 class="font-sans font-bold text-xl lg:text-7xl xl:text-[100px] xl:mt-20 xl:pb-8">
+            HMIT</h1>
+        </div>
+
+        <div class="flex flex-row items-center justify-center relative ">
+          <img class="hidden h-2 xl:absolute xl:block -left-24 z-0" src="/abouthmit/leftline.png" alt="">
+          <img class="mx-auto max-w-full w-auto h-auto z-10 mt-4 xl:mt-0" src="/abouthmit/about.png" alt="">
+          <img src="/abouthmit/love.png" class=" hidden ml-4  xl:block " alt="">
+        </div>
+        <div>
+          <p class="pt-8 xl:pt-16 text-lg text-center xl:text-left md:text-md">
+            ARA (A Renewal Agent) 4.0 adalah kegiatan yang diselenggarakan oleh HMIT (Himpunan Mahasiswa
+            Teknologi
+            Informasi) ITS periode 2020-2021 yang dimana event ini akan menjadi media untuk menyalurkan
+            minat di
+            bidang IT (teknologi informasi) bagi siswa SMA/SMK dan mahasiswa.
+          </p>
+        </div>
+
+      </div>
+
+      <!-- Right Side -->
+      <div class="flex xl:w-1/2 items-center justify-center relative">
+        <img class="mt-10 object-center xl:absolute w-28 h-28 md:w-32 md:h-32 xl:w-96 xl:h-96" src="/abouthmit/HMIT.png"
+          alt="">
+        <img class="hidden xl:block xl:relative xl:top-20 xl:left-48" src="/abouthmit/girl.png" alt="">
+      </div>
+    </div>
+
+    <div class="flex items-center justify-center xl:flex-col xl:relative">
+      <img class="hidden xl:block mb-8" src="/abouthmit/foot.png" alt="">
+    </div>
+  </div>
+
+  <!-- Section 2  -->
+  <div style="background-image: url('./abouthmit/bgsection2.png');">
+    <div class="p-6 md:p-8 items-center justify-center">
+
+      <div class="flex flex-row justify-end">
+        <img src="./abouthmit/redrectangleabouthmit.png" class="right-0 hidden lg:inline " alt="">
+      </div>
+
+      <div class="flex flex-col-reverse xl:flex-row rounded-l-xl mx-0 ">
+        <!-- Image -->
+        <div class="xl:w-1/3 relative mx-auto flex flex-col ">
+          <div class="relative ml-auto">
+            <img class="absolute -left-4 hidden lg:block" src="/abouthmit/blueplay.png" alt="">
+            <img src="/abouthmit/yellowimg.png" alt="" class="my-4 " />
+          </div>
+
+          <img src="/abouthmit/yellowimg.png" alt="" class="my-4 ml-auto" />
+        </div>
+
+
+        <!-- Content -->
+        <div class="pt-0 md:pt-2 xl:w-2/3 w-full xl:pl-20 flex-row mx-auto md:p-12">
+          <h3 class="text-[#339969]">Lorem Ipsum</h3>
+          <div class="flex flex-row items-center md:flex-row space-y-0">
+            <div class="font-sans mb-5 text-2xl md:text-3xl font-bold">Mmm, ARA? 😲
+            </div>
+          </div>
+
+          <p class="xl:w-2/3 my-4 text-lg w-full text-start text-black font-sans">
+            ARA atau A Renewal Agent merupakan sebuah kegiatan yang diselenggarakan oleh Himpunan
+            Mahasiswa
+            Teknologi Informasi ITS periode 2020-2021. Kegiatan ARA atau A Renewal Agent diadakan
+            sebagai
+            upaya untuk memberikan wadah bagi khususnya siswa SMA/SMK dan mahasiswa untuk menyalurkan
+            minat
+            dan bakatnya di bidang Internet of Things dan Cyber Security (keamanan siber), dalam rangka
+            meningkatkan kreativitas dan inovasi generasi muda untuk mendukung Pembangunan Infrastruktur
+            Teknologi untuk Indonesia
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row justify-end w-full mb-6 -top-4"
+      style="background-image: url('./abouthmit/bgsection2.png');">
+      <img src="/abouthmit/bluestick.png" class="right-0 invisible lg:visible " alt="">
+    </div>
+  </div>
+
+
+  <!-- Section 3 -->
+  <div>
+    <div class="p-6 md:p-8 items-center justify-center">
+
+      <div class="flex flex-row justify-end">
+        <img src="./abouthmit/redrectangleabouthmit.png" class="right-0 hidden lg:inline " alt="">
+      </div>
+
+      <div class="flex flex-col xl:flex-row rounded-l-xl mx-auto">
+
+        <!-- Content -->
+        <div class="pt-0 md:pt-2 xl:w-2/3 md:p-12 flex-row mx-auto w-full">
+          <h3 class="text-[#339969]">Lorem Ipsum</h3>
+          <div class="flex flex-row items-center md:flex-row space-y-0">
+            <div class="font-sans mb-5 text-2xl md:text-3xl font-bold">The Philosophy 🧐
+            </div>
+          </div>
+
+          <p class="xl:w-2/3 my-4 text-lg w-full text-start text-black font-sans">
+            ARA atau A Renewal Agent merupakan sebuah kegiatan yang diselenggarakan oleh Himpunan
+            Mahasiswa
+            Teknologi Informasi ITS periode 2020-2021. Kegiatan ARA atau A Renewal Agent diadakan
+            sebagai
+            upaya untuk memberikan wadah bagi khususnya siswa SMA/SMK dan mahasiswa untuk menyalurkan
+            minat
+            dan bakatnya di bidang Internet of Things dan Cyber Security (keamanan siber), dalam rangka
+            meningkatkan kreativitas dan inovasi generasi muda untuk mendukung Pembangunan Infrastruktur
+            Teknologi untuk Indonesia
+          </p>
+        </div>
+
+        <!-- Image -->
+        <div class="xl:w-1/3 mx-auto relative">
+          <img class="absolute invisible xl:visible -left-4" src="/abouthmit/blueplay.png" alt="">
+          <img src="/abouthmit/yellowimg.png" alt="" class="my-4" />
+          <img src="/abouthmit/yellowimg.png" alt="" class="my-4" />
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row justify-end rotate-180">
+      <img src="/abouthmit/bluestick.png" class="right-0 hidden lg:inline " alt="">
+    </div>
+  </div>

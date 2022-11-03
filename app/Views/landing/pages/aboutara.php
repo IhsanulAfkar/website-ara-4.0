3605e51be72e9e706133d7fae70a2ab5bbcd779e
ranggaaldosas <ranggaaldosas@gmail.com>
2022-11-03 14:28:10 +0700

    add aboutara

---
+++ app/Views/landing/pages/aboutara.php
@@ -0,0 +1,112 @@
+<!-- Section 1 -->
+<div style="background-image: url('./aboutara/sidebarara.png');"
+    class="flex flex-col items-center justify-center bg-white-50 text-center bg-no-repeat bg-top sm:bg-contain xl:bg-contain">
+    <h1 class="font-poppins font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-9xl mt-20 w-full h-full ">
+      A Renewal Agent <br />
+      4.0 </h1>
+    <div>
+      <img class=" w-24 sm:w-28 md:w-[170px] mt-10" src="./aboutara/aboutara.png" alt="">
+    </div>
+
+    <div class="mt-10 mb-14 w-44 sm:w-72 text-lg text-center xl:mb-56">
+      <p>
+        ARA (A Renewal Agent) 4.0 adalah kegiatan yang diselenggarakan oleh HMIT (Himpunan Mahasiswa Teknologi
+        Informasi) ITS periode 2020-2021 yang dimana event ini akan menjadi media untuk menyalurkan minat di
+        bidang IT (teknologi informasi) bagi siswa SMA/SMK dan mahasiswa.
+      </p>
+    </div>
+  </div>
+
+  <!-- Section 2  -->
+  <div style="background-image: url('./aboutara/bgsection2.png');">
+    <div class="p-6 md:p-8 items-center justify-center">
+
+      <div class="flex flex-row justify-end">
+        <img src="./aboutara/redrectangleaboutara.png" class="right-0 hidden lg:inline " alt="">
+      </div>
+
+      <div class="flex flex-col-reverse xl:flex-row rounded-l-xl mx-0 ">
+        <!-- Image -->
+        <div class="xl:w-1/3 relative mx-auto flex flex-col ">
+          <div class="relative ml-auto">
+            <img class="absolute -left-4 hidden lg:block" src="/aboutara/blueplay.png" alt="">
+            <img src="/aboutara/yellowimg.png" alt="" class="my-4 " />
+          </div>
+
+          <img src="/aboutara/yellowimg.png" alt="" class="my-4 ml-auto" />
+        </div>
+
+
+        <!-- Content -->
+        <div class="pt-0 md:pt-2 xl:w-2/3 w-full xl:pl-20 flex-row mx-auto md:p-12">
+          <h3 class="text-[#339969]">Lorem Ipsum</h3>
+          <div class="flex flex-row items-center md:flex-row space-y-0">
+            <div class="font-poppins mb-5 text-2xl md:text-3xl font-bold">Mmm, ARA? 😲
+            </div>
+          </div>
+
+          <p class="xl:w-2/3 my-4 text-lg w-full text-start text-black font-poppins">
+            ARA atau A Renewal Agent merupakan sebuah kegiatan yang diselenggarakan oleh Himpunan
+            Mahasiswa
+            Teknologi Informasi ITS periode 2020-2021. Kegiatan ARA atau A Renewal Agent diadakan
+            sebagai
+            upaya untuk memberikan wadah bagi khususnya siswa SMA/SMK dan mahasiswa untuk menyalurkan
+            minat
+            dan bakatnya di bidang Internet of Things dan Cyber Security (keamanan siber), dalam rangka
+            meningkatkan kreativitas dan inovasi generasi muda untuk mendukung Pembangunan Infrastruktur
+            Teknologi untuk Indonesia
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row justify-end w-full mb-6 -top-4"
+      style="background-image: url('./aboutara/bgsection2.png');">
+      <img src="/aboutara/bluestick.png" class="right-0 invisible lg:visible " alt="">
+    </div>
+  </div>
+
+
+
+  <!-- Section 3 -->
+  <div>
+    <div class="p-6 md:p-8 items-center justify-center">
+
+      <div class="flex flex-row justify-end">
+        <img src="./aboutara/redrectangleaboutara.png" class="right-0 hidden lg:inline " alt="">
+      </div>
+
+      <div class="flex flex-col xl:flex-row rounded-l-xl mx-auto">
+
+        <!-- Content -->
+        <div class="pt-0 md:pt-2 xl:w-2/3 md:p-12 flex-row mx-auto w-full">
+          <h3 class="text-[#339969]">Lorem Ipsum</h3>
+          <div class="flex flex-row items-center md:flex-row space-y-0">
+            <div class="font-poppins mb-5 text-2xl md:text-3xl font-bold">The Philosophy 🧐
+            </div>
+          </div>
+
+          <p class="xl:w-2/3 my-4 text-lg w-full text-start text-black font-poppins">
+            ARA atau A Renewal Agent merupakan sebuah kegiatan yang diselenggarakan oleh Himpunan
+            Mahasiswa
+            Teknologi Informasi ITS periode 2020-2021. Kegiatan ARA atau A Renewal Agent diadakan
+            sebagai
+            upaya untuk memberikan wadah bagi khususnya siswa SMA/SMK dan mahasiswa untuk menyalurkan
+            minat
+            dan bakatnya di bidang Internet of Things dan Cyber Security (keamanan siber), dalam rangka
+            meningkatkan kreativitas dan inovasi generasi muda untuk mendukung Pembangunan Infrastruktur
+            Teknologi untuk Indonesia
+          </p>
+        </div>
+
+        <!-- Image -->
+        <div class="xl:w-1/3 mx-auto relative">
+          <img class="absolute invisible xl:visible -left-4" src="/aboutara/blueplay.png" alt="">
+          <img src="/aboutara/yellowimg.png" alt="" class="my-4" />
+          <img src="/aboutara/yellowimg.png" alt="" class="my-4" />
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row justify-end rotate-180">
+      <img src="/aboutara/bluestick.png" class="right-0 hidden lg:inline " alt="">
+    </div>
+  </div>

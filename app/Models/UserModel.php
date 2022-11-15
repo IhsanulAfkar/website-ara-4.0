5163ffad65dfb3b705ee0f8373c3029ee0cbe969
Tridiktya Putra <tridiktya@gmail.com>
2022-11-15 13:42:41 +0700

    add dashboard for admin olim

---
+++ app/Models/UserModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    // ...
+    protected $table = 'user';
+    protected $primaryKey = 'user_id';
+    protected $allowedFields  = [
+        'user_lomba', 
+        'user_tim_nama', 
+        'user_username', 
+        'user_password'
+    ];
+    protected $user_lomba = 'user_lomba';
+    protected $user_tim_nama = 'user_tim_nama';
+    protected $user_username = 'user_username';
+    protected $user_password = 'user_password';
+    
+    protected $useTimestamps = true;
+    protected $createdField = 'user_date_created';
+    protected $updatedField = 'user_date_updated';
+}
